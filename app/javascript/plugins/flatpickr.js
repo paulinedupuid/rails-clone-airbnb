9b518c1976049074064fc9f0b6927df4d0280101
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-02-03 16:44:18 +0100

    moved variables to function in flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,17 +1,13 @@
 import flatpickr from "flatpickr";
 
-let availability = gon.flatavailability;
-let reservation = gon.flatreservation;
-
 const initFlatpickr = () => {
+  let availability = gon.flatavailability;
+  let reservation = gon.flatreservation;
   flatpickr(".datepicker", {
     minDate: availability.at(0),
     maxDate: availability.at(-1),
     disable: reservation
   });
-  console.log(availability.at(0));
-  console.log(availability.at(-1));
-
 }
 
 export { initFlatpickr };
