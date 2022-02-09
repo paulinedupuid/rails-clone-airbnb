955069950cec209656b863de411c36f8f6b64290
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-02-09 19:42:14 +0100

    trying to allow input to be cleared

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :flat
   validates :reservation_start, presence: true
-  validates :reservation_end, presence: true
+  validates :reservation_end, presence: true, numericality: { greater_than: :reservation_start }
   validates :number_of_guests, presence: true
   # validate :end_date_is_after_start_date
 
